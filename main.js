ab965ea26781024e1258a1670eaee7190dd8fbe0
TCO932 <chirkof.02@gmail.com>
2024-09-01 05:47:24 +0400

    mobile-0.9

---
+++ main.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('subscribe-form')
+const subscribe = document.getElementById('subscribe')
+const success = document.getElementById('success')
+
+function handleSubmit(e) {
+    e.preventDefault()
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    console.log(data)
+
+    const emailInput = document.getElementById('email');
+    const errorMessage = document.getElementById('error-message');
+
+    if (!emailInput.checkValidity()) {
+        errorMessage.textContent = emailInput.validationMessage;
+        errorMessage.style.display = 'block';
+        emailInput.classList.add('submited')
+    } else {
+        errorMessage.style.display = 'none';
+        subscribe.style.display = 'none';
+        success.style.display = 'flex';
+        const userEmail = document.getElementById('user-email')
+        userEmail.textContent = emailInput.value
+    }
+}
+
+form.addEventListener('submit', handleSubmit)
